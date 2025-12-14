3e53c3b2703794f91844b9d37533a5af5b6e94eb
gnk@delegate.dk <gnk@delegate.dk>
2025-12-14 12:35:03 +0100

    Add swagger to production

--- RisagerBackend/Program.cs
+++ RisagerBackend/Program.cs
@@ -122,17 +122,8 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
-}
-else
-{
-    // Only redirect to HTTPS in production
-    app.UseHttpsRedirection();
-}
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
 
 // Use CORS
 app.UseCors("AllowFrontend");
